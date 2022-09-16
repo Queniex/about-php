5cde613d3293d16e41c345057a3bd66126b84d8d
Queniex <queniesalbiyah@gmail.com>
2022-09-16 20:41:18 +0700

    about array

---
+++ array.php
@@ -0,0 +1,14 @@
+<?php
+// array
+// array could contain many value
+
+// old method
+$day = array("monday", "tuesday");
+
+// new method
+$month = ["jan", "feb", "march"];
+
+//array could containt many tipe of value
+$arr1 = [123, "text"];
+
+?>
