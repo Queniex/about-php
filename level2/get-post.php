0bdbe93cbad0d0157bd78e8ba41e36cebc115aaf
Queniex <queniesalbiyah@gmail.com>
2022-09-16 22:40:32 +0700

    adding image file

---
+++ level2/get-post.php
@@ -0,0 +1,14 @@
+<?php
+// it was empty b4
+// but, with this way it could be filled
+// $_GET["name"] = "Quenie Salbiyah";
+// $_GET["gender"] = "Girl";
+var_dump($_GET);
+
+$animals = [
+    ["name" => "cat", "type" => "carnivore", "leg" => 4, "image" => "img/cat.jpg.png"],
+    ["name" => "cow", "type" => "herbivore", "leg" => 4, "image" => "img/cow.jpg.png"],
+    ["name" => "monkey", "type" => "omnivore", "leg" => 2, "image" => "img/monkey.jpg.png"]
+]
+
+?>
