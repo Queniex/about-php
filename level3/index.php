ef972652bf3c7a997c7948bb50441255d8809315
Queniex <queniesalbiyah@gmail.com>
2022-09-17 20:22:49 +0700

    make it bold

--- level3/index.php
+++ level3/index.php
@@ -13,11 +13,11 @@
 <table border="1" cellpadding="10" cellspacing="0">
 
     <tr>
-        <td>No.</td>
-        <td>Aksi</td>
-        <td>Gambar</td>
-        <td>Nim</td>
-        <td>Nama</td>
+        <th>No.</th>
+        <th>Aksi</th>
+        <th>Gambar</th>
+        <th>Nim</th>
+        <th>Nama</th>
     </tr>
 
 </table>
