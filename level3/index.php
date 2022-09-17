653a52779c5d1a89b3843eea12f4eb66c7238d1f
Queniex <queniesalbiyah@gmail.com>
2022-09-17 20:21:28 +0700

    abstract

---
+++ level3/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+<body>
+    
+<h1>Daftar Mahasiswa</h1>
+
+<table border="1" cellpadding="10" cellspacing="0">
+
+    <tr>
+        <td>No.</td>
+        <td>Aksi</td>
+        <td>Gambar</td>
+        <td>Nim</td>
+        <td>Nama</td>
+    </tr>
+
+</table>
+
+</body>
+</html>
