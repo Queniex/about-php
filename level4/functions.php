d1a880dd23f690418a1ac1be7a616dea8c28a2ab
Queniex <queniesalbiyah@gmail.com>
2022-09-18 06:10:37 +0700

    add delete function

--- level4/functions.php
+++ level4/functions.php
@@ -1,7 +1,7 @@
 <?php
 $conn = mysqli_connect("localhost", "root", "", "phpdasar");
 
-function query($query){
+function query($query) {
     global $conn;
     $result = mysqli_query($conn, $query);
     $rows = [];
@@ -11,7 +11,7 @@ function query($query){
     return $rows;
 }
 
-function add($data){
+function add($data) {
     global $conn;
     $nim = htmlspecialchars($data["Nim"]);
     $nama = htmlspecialchars($data["Nama"]);
@@ -25,5 +25,11 @@ function add($data){
     return mysqli_affected_rows($conn);
 }
 
+function deletes($id) {
+    global $conn;
+    mysqli_query($conn, "DELETE FROM mahasiswa WHERE Id = $id");
+
+    return mysqli_affected_rows($conn);
+}
 
 ?>
